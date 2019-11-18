8aabca5c46f0fd2685ed98a4f0e866e52ae857aa
David Tauraso <dtauraso@gmail.com>
2019-11-18 10:28:48 -0800

    Put in quiz interface outline

---
+++ celeb-dead-or-alive/src/components/CelebrityDeadOrAliveQuiz.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CelebrityDeadOrAliveQuiz() {
+
+
+    return (
+
+        <div>
+            <p>here is our image</p>
+            <p>here is our name</p>
+            <button>Dead</button>
+            <button>Alive</button>
+        </div>
+
+    );
+}
