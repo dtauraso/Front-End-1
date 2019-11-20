e5049ec415c1aa6320493be99a5c8c93c3874e85
David Tauraso <dtauraso@gmail.com>
2019-11-20 11:28:02 -0800

    Added a basic login form w formik and the reqres api

--- celeb-dead-or-alive/src/components/CelebrityDeadOrAliveQuiz.js
+++ celeb-dead-or-alive/src/components/CelebrityDeadOrAliveQuiz.js
@@ -168,7 +168,7 @@ function CelebrityDeadOrAliveQuiz(props) {
 
     }
     // put in readme
-    {/* yarn add styled-components, axios, react-router-dom */}
+    {/* yarn add styled-components, axios, react-router-dom, formik, yup */}
 
     // About Wunderlist 2.0
 // Wunderlist 2.0 takes your traditional to-do list app and automates it. It not only allows you to write your to-do, work, grocery, trip and household lists, but allows you to set up recurring to do lists by date and time. (Think recurring events on google calendar + to-do list app). No matter how much is on your plate, Wunderlist 2.0 makes it super easy to remember all the little recurring to-dos and surprise to-dos that pop up unexpectedly.
---
+++ celeb-dead-or-alive/src/components/Login.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react"
+import { withFormik, Form, Field } from "formik"
+import * as Yup from "yup";
+import axios from "axios";
+
+
+// stuff for formik to use
+const MyForm = ( {values, errors, touched, status, handleChange } ) => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        if(status) {
+            setUsers([...users, status])
+        }
+    }, [status])
+
+    return (
+
+        <div className="userForm">
+
+
+            <Form>
+                <Field
+                    type="text"
+                    name="username"
+                    placeholder="username"/>
+                {/* if the user clicked into the name field, made an error when filling it out
+                print out an error message */}
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+                <button>Submit</button>
+            </Form>
+            {users.map(user => (
+               <ul key={user.id}>
+                <li>Username: {user.username}</li>
+                
+               </ul>
+                
+            ))}
+        </div>
+    )
+}
+
+const FormikUserForm = withFormik({
+    // user filling out form
+    mapPropsToValues({username}) {
+        // holding the state of the form
+        // each filed can have a default "0" value or whatever the user types in
+
+        return {
+            username: username || ""
+
+        };
+    },
+    // validating what the user typed in
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+        .min(2, 'Too short!')
+        .max(50, 'Too Long!')
+        .required('a username is required')
+    }),
+    // send the user data to the server
+    // same values as above
+    handleSubmit(values, { setStatus, resetForm }) {
+        axios   .post("https://reqres.in/api/users/", values)
+                .then(res => {
+                    console.log("received", res.data)
+                    setStatus(res.data)
+                    resetForm({})
+                })
+                .catch(err => console.log(err.response));
+    }
+
+})(MyForm);
+export default FormikUserForm;
+console.log("This is the HOC", FormikUserForm);
