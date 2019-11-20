4c6e629eb53d6cf9be15547702f09f15a89cc052
Rebecca Joyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-11-20 13:21:03 -0500

    merge to master

--- celeb-dead-or-alive/src/App.js
+++ celeb-dead-or-alive/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import LogIn from './components/users/LogIn';
+import SignUp from '../src/components/SignUp';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axiosWithAuth from './Component/utils/axiosWithAuth';
+import PrivateRoute from './Component/utils/PrivateRoute';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends Component {
+	state = {
+		data: []
+	};
+
+	
+
+
+		return (
+
+		
+			<Router>
+		
+				<Route exact path="/SignUp" component={SignUp} />
+				<Route exact path="/login" component={LogIn} />
+			</Router>
+	
+		);
+    }
+
+  
 
 export default App;
