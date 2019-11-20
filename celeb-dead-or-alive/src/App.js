068ddf02fd3f0bc3f7a701df65acb6efa73563a4
Hunter5555 <hunter.m.allan@gmail.com>
2019-11-20 10:18:03 -0700

    started form

--- celeb-dead-or-alive/src/App.js
+++ celeb-dead-or-alive/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Timer from './Timer'
+import NewUser from "./Form"
+
 
 class App extends Component{
   render(){
