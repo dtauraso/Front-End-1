4495e803d9352d54fc2946b868535ac8e61b07b9
David Tauraso <dtauraso@gmail.com>
2019-11-21 14:13:30 -0800

    Adjusted nav bar positioning

--- celeb-dead-or-alive/src/App.js
+++ celeb-dead-or-alive/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
 
-      <nav>
+      <nav className="NavBar">
         <Link to="/quiz" onClick={() => {
           setPlayerScore(0)
           
